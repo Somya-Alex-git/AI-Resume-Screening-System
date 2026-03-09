284f74bb1b05bcd18e568928ed30dcdf291b458b
Somya-Alex-git <somya.24bca7725@vitapstudent.ac.in>
2026-03-09 23:08:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,19 +4,27 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 st.title("AI Resume Screening System")
 
-st.write("Paste a resume and job description to calculate match score.")
+st.write("Upload your resume and paste a job description to check the match score.")
 
-resume_text = st.text_area("Resume Text")
-job_text = st.text_area("Job Description")
+# Upload resume file
+uploaded_file = st.file_uploader("Upload Resume (TXT)", type=["txt"])
+
+# Job description input
+job_description = st.text_area("Paste Job Description")
 
 if st.button("Calculate Match Score"):
-    documents = [resume_text, job_text]
+    if uploaded_file is not None and job_description != "":
+        resume_text = uploaded_file.read().decode("utf-8")
 
-    vectorizer = TfidfVectorizer()
-    vectors = vectorizer.fit_transform(documents)
+        documents = [resume_text, job_description]
 
-    similarity = cosine_similarity(vectors[0], vectors[1])[0][0]
+        vectorizer = TfidfVectorizer()
+        vectors = vectorizer.fit_transform(documents)
 
-    score = round(similarity * 100, 2)
+        similarity = cosine_similarity(vectors[0], vectors[1])[0][0]
+        score = round(similarity * 100, 2)
 
-    st.success(f"Match Score: {score}%")
+        st.success(f"Match Score: {score}%")
+
+    else:
+        st.warning("Please upload a resume and enter a job description.")
